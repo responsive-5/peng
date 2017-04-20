b1797fe728e4408aebe585d4374e06086b4b9f93
houyangy <18435106280@163.com>
2017-04-21 03:05:01 +0800

    3:04

--- 宠物/js/h_js/h_lckt.js
+++ 宠物/js/h_js/h_lckt.js
@@ -24,10 +24,10 @@ $(function(){
         $('.big ul li').hover(function(){
             $(this).css({background:'#E6E6E6',borderRadius:'12px',marginTop:-20}).children().eq(0).css({
                 display:'block',
-                width:76,
-                height:40,
+                width:'80%',
+                height:"60%",
                 borderRadius:'6px',
-                lineHeight:'42px',
+                lineHeight:'40px',
                 textAlian:'center',
                 position:'absolute',
                 top:0,
@@ -37,7 +37,7 @@ $(function(){
                 margin:'auto',
                 zIndex:999,
                 color:'#2EB6BE',
-                fontSize:15,
+                fontSize:"15%",
                 background:'#EDEDED',
             });
             $('<div><img src="img/m-img/nav-a.png" alt="" style="width:100%;height:100%;"></div>').appendTo($(this))
