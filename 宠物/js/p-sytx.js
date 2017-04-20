dbe64987454228f1479f1f3b1036bb58b58e704c
Martinqi <965593718@qq.com>
2017-04-20 16:44:46 +0800

    'xinxin'

--- 宠物/js/p-sytx.js
+++ 宠物/js/p-sytx.js
@@ -12,4 +12,19 @@ $(document).ready(function () {
         $(this).toggleClass('glyphicon glyphicon-menu-left')
 
     })
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+        }
+    });
 });
