9c6767c82c97c0a525ec64bd0eaad9e7464cdbf4
Martinqi <965593718@qq.com>
2017-04-22 10:34:21 +0800

    'dsda'

--- 宠物/js/p-gywm.js
+++ 宠物/js/p-gywm.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/4/22.
  */
 window.onload=function () {
-    window.onresize=function () {
+    $(window).resize(function () {
         let pw=document.documentElement.clientWidth;
         if(pw<400){
             $('.biankuan').removeClass('col-xs-6')
@@ -11,6 +11,6 @@ window.onload=function () {
             $('.biankuan').removeClass('col-xs-12')
             $('.biankuan').addClass('col-xs-6')
         }
-    };
+    })
     $(window).triggerHandler('resize');
 };
